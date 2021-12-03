30ed105c3a62e081c7c89e38ae9e3993585fbc38
Anwesha Mishra <anwesharoses@gmail.com>
2021-12-03 13:34:02 +0800

    added hash password, salt, authtoken
    Made password more secure by hashing our passowrd, added salt to make it more secure and using Jason web token, generated token for user id

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,10 @@ const express=require('express');
 const router=express.Router();
 const User=require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs'); // imported bcrypt js
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET="Anwesha*roses";
 
 //create a user using: POST "/api/auth/createuser". Doesnt require Auth
 
@@ -23,13 +27,20 @@ router.post('/createuser',[
         if (user){
             return res.status(400).json({ error:"Sorry a user with this email already exsists"});
         }
+        const salt= await bcrypt.genSalt(10);
+        const secPacc=  await bcrypt.hash(req.body.password,salt);
         //create a new user
         user=await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: secPacc,
         })
-        res.json(user)
+        const data={
+            id:user.id
+        }
+        const authtoken=jwt.sign(data,JWT_SECRET);
+        res.json({authtoken})
+        // res.json(user)
     } catch(error){
         console.error(error.message);
         res.status(500).send("Some error occured")
