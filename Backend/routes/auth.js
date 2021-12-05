b9e265cafebca62aea6155b7f7d0c600891dfb2e
Anwesha Mishra <anwesharoses@gmail.com>
2021-12-05 11:06:19 +0800

    added email id and password verification

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -7,7 +7,7 @@ var jwt = require('jsonwebtoken');
 
 const JWT_SECRET="Anwesha*roses";
 
-//create a user using: POST "/api/auth/createuser". Doesnt require Auth
+//ROUTE 1:create a user using: POST "/api/auth/createuser". Doesnt require Auth
 
 router.post('/createuser',[
     body('name','Enter a valid mail').isLength({min:3}), 
@@ -18,11 +18,11 @@ router.post('/createuser',[
     //if there are errors return bad requests and the erros
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ errors: errors.array() });
     }
     //check whether the user with the same email exsists already
     try{
-
+        
         let user= await User.findOne({email:req.body.email});
         if (user){
             return res.status(400).json({ error:"Sorry a user with this email already exsists"});
@@ -43,8 +43,45 @@ router.post('/createuser',[
         // res.json(user)
     } catch(error){
         console.error(error.message);
-        res.status(500).send("Some error occured")
+        res.status(500).send("Internal sever error")
     }
 })
+// ROUTE 2: Authentic a user using: POST "/api/auth/login". Doesnt require Auth
+// Asking user to enter email and password
+router.post('/login',[
+    body('email','Enter a valid Email').isEmail(),
+    body('password','Password cannot be blank').exists(),
+    
+],async(req,res)=>{
+    //if there are errors return bad requests and the erros
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+// checking if the emailid and password is correct
+    const{email, password}=req.body;
+    try{
+        let user=await User.findOne({email});
+        // if user does not exsists then show error
+        if(!user){
+            return res.status(400).json({error:"Please login with correct credentials"});
+        }
+        const passwordCompare= await bcrypt.compare(password,user.password);
+        // if password does not exsists then show error
+        if(!passwordCompare){
+            return res.status(400).json({error:"Please login with correct credentials"});
+        }
+        //using the id generate the auth token
+        const data={
+            id:user.id
+        }
+        const authtoken=jwt.sign(data,JWT_SECRET);
+        res.json({authtoken})
+    }catch(error){
+        //if auth token cannot be generated after correct id and password then throw this error
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
 
+})
 module.exports=router
