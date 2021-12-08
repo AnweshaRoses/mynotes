5cec45c29795132f268ba5729fd7905371b637c0
Anwesha Mishra <anwesharoses@gmail.com>
2021-12-08 11:31:10 +0800

    Set up React Router dom and added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello this is Anwesha</h1>
-    </div>
+    <>
+    {/* After the new update you dont need to use exact tag and switch has been changed to Routes */}
+    <Router>
+    <Navbar/>
+     <Routes>
+          <Route path="/"  element={<Home/>}>
+          </Route>
+          <Route  path="/about" element={<About/>}>
+          </Route>
+        </Routes>
+    </Router>
+    </>
   );
 }
 
